0ae6125e4ad309fc8a63b64fec6254ccc625db84
Seth Samuel <sethsamueldev@gmail.com>
2026-01-14 22:20:27 +1300

    feat: add description of Arachne project to HomePage for better context

--- frontend/src/app/[locale]/page.tsx
+++ frontend/src/app/[locale]/page.tsx
@@ -32,6 +32,23 @@ export default function HomePage() {
               </div>
             </CardContent>
           </Card>
+
+          <Card className="mt-8 flex flex-col items-center">
+            <CardHeader>
+              <CardTitle>What is Arachne?</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <p className="mb-4 text-center text-muted-foreground">
+                Arachne is my personal project to build a research assistant. My idea was that i wanted my own jarvis from iron man with me so i can ask it questions and help me find information. I wanted to rival the expensive research assistants from OpenAi, Google and perplexity and build my own.
+              </p>
+              <p className="mb-4 text-center text-muted-foreground">
+                After completing some study to get back into the IT space i found the wonders of AI. It has shown me many things and surprised me at almost every turn. Some research argues that ai is not sentient but i have to disagree. The way it speaks, understand and behaves is like it understands even if its just numbers at the end. There is nothing to prove humans do not operate the same way with chemical signals driving them. 
+              </p>
+              <p>
+                This is my gift to myself and the world to help people do research better and faster and to prove if you want to you can do anything you want. 
+              </p>
+            </CardContent>
+          </Card>
         </div>
       </div>
     </div>
